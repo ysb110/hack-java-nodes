663ce4357eab6fb57af0ab3b51d7c8d316faf837
ysb110 <165580405@qq.com>
2019-10-15 04:59:51 +0800

    首次提交3

--- ycq-controller/src/main/java/com/ycq/ycqcontroller/YcqControllerApplication.java
+++ ycq-controller/src/main/java/com/ycq/ycqcontroller/YcqControllerApplication.java
@@ -2,12 +2,18 @@ package com.ycq.ycqcontroller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class YcqControllerApplication {
+public class YcqControllerApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YcqControllerApplication.class, args);
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(YcqControllerApplication.class);
 	}
 
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(YcqControllerApplication.class, args);
+	}
 }
--- ycq-user/src/main/java/com/ycq/ycquser/YcqUserApplication.java
+++ ycq-user/src/main/java/com/ycq/ycquser/YcqUserApplication.java
@@ -2,12 +2,18 @@ package com.ycq.ycquser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class YcqUserApplication {
+public class YcqUserApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YcqUserApplication.class, args);
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(YcqUserApplication.class);
 	}
 
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(YcqUserApplication.class, args);
+	}
 }
