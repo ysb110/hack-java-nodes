2fb17923e7bd4a62bdf2d96dbd10b09d93b2c5bc
ysb110 <165580405@qq.com>
2020-01-11 13:19:54 +0800

    首次提交311

--- ycq-common-service-facade/src/main/java/com/ycq/ycqcommonservicefacade/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.ycq.ycqcommonservicefacade;
-
-import com.ycq.ycqcommonservicefacade.vo.LoginReq;
-import com.ycq.ycqcommonservicefacade.vo.UserEntity;
-
-public interface UserService {
-    public UserEntity login(LoginReq loginReq);
-}
